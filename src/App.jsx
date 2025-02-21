9255552596fcec08b8d14afbb37336e4ce8c75a4
arzerra <ianclark1506@gmail.com>
2025-02-21 16:00:43 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "@/layouts/MainLayout";
+import Todos from "./pages/Todos";
+import Todo from "./pages/Todo";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Todos/>} />
+      <Route path="/todo/:id" element={<Todo/>} />
+    </Route>
   )
-}
+);
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/components/ClickMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const ClickMe = () => {
+  return (
+    <div >
+        <Button variant="ghost">Click Me</Button>
+    </div>
+  );
+};
+
+export default ClickMe
